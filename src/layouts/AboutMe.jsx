9001d7d0e61de7522f7dd1c8ffcce893a3b03a00
DavidClaudio03 <davidclaudio5000@gmail.com>
2025-01-02 21:54:07 -0500

    Ajuste de colores

--- src/layouts/AboutMe.jsx
+++ src/layouts/AboutMe.jsx
@@ -14,14 +14,14 @@ export default function About() {
   ]
 
   return (
-    <section id="about" className="py-10 bg-white">
-      <div className="py-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border-2 border-gray-200 bg-gray-50 rounded-lg">
+    <section id="about" className="py-10">
+      <div className="py-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border-2 border-gray-200 rounded-lg">
         <div className="">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase mt-2">Sobre mí</h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-indigo-600 sm:text-4xl">
             Pasión por el desarrollo y la innovación
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 ">
+          <p className="mt-4 max-w-2xl text-xl text-white">
             Mi trayectoria se caracteriza por una combinación de creatividad, adaptabilidad y un firme
             compromiso con el aprendizaje continuo.
           </p>
@@ -35,9 +35,9 @@ export default function About() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                 </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Fortalezas Profesionales</p>
+                <p className="ml-16 text-lg leading-6 font-medium text-indigo-600">Fortalezas Profesionales</p>
               </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
+              <dd className="mt-2 ml-16 text-base text-white">
                 Creatividad en la resolución de problemas, comunicación efectiva, alta adaptabilidad,
                 trabajo en equipo y compromiso con la responsabilidad.
               </dd>
@@ -49,9 +49,9 @@ export default function About() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
                   </svg>
                 </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Valores Personales</p>
+                <p className="ml-16 text-lg leading-6 font-medium text-indigo-600">Valores Personales</p>
               </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
+              <dd className="mt-2 ml-16 text-base text-white">
                 Responsabilidad, aprendizaje continuo, colaboración, mejora personal constante y
                 apoyo a los compañeros.
               </dd>
@@ -71,10 +71,10 @@ export default function About() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-4">
           <div className="lg:text-center">
             <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Habilidades</h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
               Habilidades Técnicas
             </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <p className="mt-4 max-w-2xl text-xl text-white lg:mx-auto">
               Mis habilidades abarcan diversas áreas del desarrollo de software, permitiéndome abordar proyectos de manera integral.
             </p>
           </div>
@@ -89,9 +89,9 @@ export default function About() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                       </svg>
                     </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{skill.name}</p>
+                    <p className="ml-16 text-lg leading-6 font-medium text-indigo-500">{skill.name}</p>
                   </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">{skill.description}</dd>
+                  <dd className="mt-2 ml-16 text-base text-white">{skill.description}</dd>
                 </div>
               ))}
             </dl>
--- src/popup/ProfessionalVision.jsx
+++ src/popup/ProfessionalVision.jsx
@@ -6,13 +6,13 @@ function ProfessionalVision({ isPopupOpen, closePopup }) {
     <Popup isOpen={isPopupOpen} onClose={closePopup}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base text-indigo-500 font-semibold tracking-wide uppercase">
             Visión
           </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
             Visión Profesional
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          <p className="mt-4 max-w-2xl text-xl text-white lg:mx-auto">
             Mi objetivo es convertirme en un desarrollador Full Stack Senior,
             con una especialización emergente en ciberseguridad y hacking
             ético.
@@ -39,11 +39,11 @@ function ProfessionalVision({ isPopupOpen, closePopup }) {
                     />
                   </svg>
                 </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                <p className="ml-16 text-lg leading-6 font-medium text-indigo-500">
                   Aspiraciones
                 </p>
               </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
+              <dd className="mt-2 ml-16 text-base text-white">
                 <ul className="list-disc pl-5 space-y-2">
                   <li>Dominar múltiples tecnologías de desarrollo</li>
                   <li>
@@ -74,11 +74,11 @@ function ProfessionalVision({ isPopupOpen, closePopup }) {
                     />
                   </svg>
                 </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                <p className="ml-16 text-lg leading-6 font-medium text-indigo-500">
                   Retos Profesionales
                 </p>
               </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
+              <dd className="mt-2 ml-16 text-base text-white">
                 <ul className="list-disc pl-5 space-y-2">
                   <li>Desarrollo de software con Inteligencia Artificial</li>
                   <li>
